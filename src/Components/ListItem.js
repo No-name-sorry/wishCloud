f321c4758d9010e509061754a9ce60e96053bab5
Viktoriia <planshetassus@gmail.com>
2024-11-22 17:35:58 +0100

    add new component Items in Lists

---
+++ src/Components/ListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Components/ListItem.css'
+
+const ListItem = () => {
+  return (
+    <div>
+      <div className='bla'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
