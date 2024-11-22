7e025609dca05af98339ad7cda3d2f4fa1827cc6
Viktoriia <planshetassus@gmail.com>
2024-11-22 17:34:27 +0100

    .

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 import langLogo from '../assets/lang-menu.png'
 import icon_lg from '../assets/icon_lg.png'
 
-
 const Navbar = () => {
+    const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
         <img src={icon_lg} alt="" className='logo_img'/>
         <ul>
-            <Link to="/"><li>Home</li></Link>
-            <Link to="/mylists"><li>My Lists</li></Link>
-            <Link to="/create"><li>Create</li></Link>
+            <Link to="/homepage"><li onClick={()=>{setMenu("home")}}>
+                Home{menu==="home"?<hr/>:<></>}</li></Link>
+            <Link to="/mylists"><li onClick={()=>{setMenu("lists")}}>
+                My Lists{menu==="lists"?<hr/>:<></>}</li></Link>
+            <Link to="/create"><li onClick={()=>{setMenu("create")}}>
+                Create{menu==="create"?<hr/>:<></>}</li></Link>
         </ul>
 
         <div className='buttons-wrapper'>
