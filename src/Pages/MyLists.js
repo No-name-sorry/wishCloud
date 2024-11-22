edaa54b209779dd5cd4c338b05b8028da57de380
Viktoriia <planshetassus@gmail.com>
2024-11-22 17:33:01 +0100

    add My List page

--- src/Pages/MyLists.js
+++ src/Pages/MyLists.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../Pages/MyLists.css'
+import List from '../Components/List.js'
 
 const MyLists = () => {
   return (
-    <div>
-      <h1>Welcome to my lists page!</h1>
+    <div className='content-MyList'>
+      <h1>My Lists</h1>
+      <section className='main-content-MyList'>
+
+        <div className='lists-div'>
+          <List/>
+          <List/>
+          <List/>
+        </div>
+        
+      </section>
     </div>
   )
 }
