08c4e4ba9872c0d72f8a3cd77db5afac6fcb53fb
Viktoriia <planshetassus@gmail.com>
2024-12-16 15:03:04 +0100

    adding settings for backend

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js"
+import { verifyToken } from "./middleware/auth.js"
  
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -24,6 +27,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+
 /* FILE STORAGE */
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -34,6 +38,14 @@ const storage = multer.diskStorage({
     },
   });
   const upload = multer({ storage });
+
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), verifyToken, register);
+
+
+/* ROUTES */
+app.use("/auth", authRoutes)
 
 
 /* MONGOOSE SETUP */
