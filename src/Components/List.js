b1490079ead10b9a3db1b9ae8920eea344af8236
Viktoriia <planshetassus@gmail.com>
2024-12-16 14:59:57 +0100

    add empty list element
    added empty line for creating new list
    
    refactoring for modules css

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,29 +1,57 @@
-import React from 'react'
-import '../Components/List.css'
+import React, { useState } from 'react'
+import {NavLink} from 'react-router-dom'
+import classes from '../Components/List.module.css'
 import cancel from '../assets/cancel.png'
 import time from '../assets/time.png'
 // import done from '../assets/done.png'
-import dropdown from '../assets/dropdown.png'
 
+export const List = () => {
+  const options = {
+    privacy: ['Public', 'Private'],
+  };
 
-const List = () => {
+  const [selectedPrivacy, setSelectedPrivacy] = useState('Public');
+
+  const handlePrivacyChange = (e) => {
+    setSelectedPrivacy(e.target.value);
+  };
+
   return (
-    <div className='list'>
-        <p className='list-title'>Summer Trip</p>
-        <div className='progress'>
+      <div className={classes.list}>
+      <NavLink to='/openlist'><p className={classes.listTitle}>Summer Trip</p></NavLink>
+        <div className={classes.progress}>
             <img src={time} alt='img'/>
             <p>In Progress</p>
         </div>
         <div className='category'>
             <p>Travelling</p>
         </div>
-        <div className='security'>
-            <p>Public</p>
-            <img src={dropdown} alt='dropdown img'/>
+        <div className={classes.security}>
+          <select
+            value={selectedPrivacy}
+            onChange={handlePrivacyChange}
+            className={classes.selectPrivacy}
+          >
+            {options.privacy.map((privacy) => (
+              <option key={privacy} value={privacy}>
+                {privacy}
+              </option>
+            ))}
+          </select>
         </div>
-        <img src={cancel} alt='cancel'/>
-    </div>
+        <img className={classes.cancelButton} src={cancel} alt='cancel'/>
+      </div>
   )
 }
 
-export default List
+export const ListAdd = () => {
+  return (
+    <NavLink to='/create'><div className={`${classes.list} ${classes.add}`}>
+        <div class={classes.plusIcon}>
+          <div class={classes.vertical}></div>
+          <div class={classes.horizontal}></div>
+        </div>
+    </div>
+    </NavLink>
+  )
+}
